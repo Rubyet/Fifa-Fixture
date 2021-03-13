4675e6148331bf8b911c348dfd1335e68823763a
Rubyet <rittick.2012@gmail.com>
2021-03-13 17:26:37 +0600

    Qualifier round done

--- custom.js
+++ custom.js
@@ -1,33 +1,31 @@
-var tpl=[0,1,2,3,4,5];
-var tw=[0,1,2,3,4,5];
-var td=[0,1,2,3,4,5];
-var tl=[0,1,2,3,4,5];
-var ts=[0,1,2,3,4,5];
-var ta=[0,1,2,3,4,5];
-var tdif=[0,1,2,3,4,5];
-var tpo=[0,1,2,3,4,5];
+var tpl = [0, 0, 0, 0, 0, 0];
+var tw = [0, 0, 0, 0, 0, 0];
+var td = [0, 0, 0, 0, 0, 0];
+var tl = [0, 0, 0, 0, 0, 0];
+var ts = [0, 0, 0, 0, 0, 0];
+var ta = [0, 0, 0, 0, 0, 0];
+var tdif = [0, 0, 0, 0, 0, 0];
+var tpo = [0, 0, 0, 0, 0, 0];
 
-var ppl=[0,1,2];
-var pw=[0,1,2];
-var pd=[0,1,2];
-var pl=[0,1,2];
-var ps=[0,1,2];
-var pa=[0,1,2];
-var pdif=[0,1,2];
-var ppo=[0,1,2];
+var qualifier = 0;
+var knockout = 0;
 
-function compare(first,second)
-{
-  if($(first).val()==$(second).val())
-  {
+var ppl = [0, 0, 0];
+var pw = [0, 0, 0];
+var pd = [0, 0, 0];
+var pl = [0, 0, 0];
+var ps = [0, 0, 0];
+var pa = [0, 0, 0];
+var pdif = [0, 0, 0];
+var ppo = [0, 0, 0];
+
+function compare(first, second) {
+  if ($(first).val() == $(second).val()) {
     return "draw";
   }
-  if($(first).val()<$(second).val())
-  {
+  if ($(first).val() < $(second).val()) {
     return second;
-  }
-  else
-  {
+  } else {
     return first;
   }
 }
@@ -56,26 +54,195 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 200);
 });
 
+function team(id) {
+  var id_name = $(id).attr("id");
+  return id_name.split("_")[0];
+}
+function draw(id1,id2){
+  var goal1 = parseInt($(id1).val());
+  var goal2 = parseInt($(id2).val());
+  for(i=0;i<6;i++)
+        {
+          console.log("1- "+i)
+          if (team(id1).includes((i+1).toString())) {
+            tpl[i]=tpl[i]+1;
+            tw[i]=tw[i];
+            td[i]=td[i]+1;
+            tl[i]=tl[i];
+            ts[i]=parseInt(ts[i])+goal1;
+            ta[i]=parseInt(ta[i])+goal2;
+            tdif[i]=ts[i]-ta[i];
+            tpo[i]=tpo[i]+1;
+            var p;
+            if(i==0 || i==1)
+            {
+              p=0;
+            }
+            if(i==2 || i==3)
+            {
+              p=1;
+            }
+            if(i==4 || i==5)
+            {
+              p=2;
+            }
+            ppl[p]=ppl[p]+1;
+            pw[p]=pw[p];
+            pd[p]=pd[p]+1;
+            pl[p]=pl[p];
+            ps[p]=parseInt(ps[p])+goal1;
+            pa[p]=parseInt(pa[p])+goal2;
+            pdif[p]=ps[p]-pa[p];
+            ppo[p]=ppo[p]+1;
+          }
+          
+          if (team(id2).includes((i+1).toString())) {
+            tpl[i]=tpl[i]+1;
+            tw[i]=tw[i];
+            td[i]=td[i]+1;
+            tl[i]=tl[i];
+            ts[i]=parseInt(ts[i])+goal2;
+            ta[i]=parseInt(ta[i])+goal1;
+            tdif[i]=ts[i]-ta[i];
+            tpo[i]=tpo[i]+1;
+            var p;
+            if(i==0 || i==1)
+            {
+              p=0;
+            }
+            if(i==2 || i==3)
+            {
+              p=1;
+            }
+            if(i==4 || i==5)
+            {
+              p=2;
+            }
+            ppl[p]=ppl[p]+1;
+            pw[p]=pw[p];
+            pd[p]=pd[p]+1;
+            pl[p]=pl[p];
+            ps[p]=parseInt(ps[p])+goal2;
+            pa[p]=parseInt(pa[p])+goal1;
+            pdif[p]=pa[p]-pa[p];
+            ppo[p]=ppo[p]+1;
+          }
+        }
+}
+function win(id1,id2){
+  var goal1 = parseInt($(id1).val());
+  var goal2 = parseInt($(id2).val());
+  for(i=0;i<6;i++)
+        {
+          if (team(id1).includes((i+1).toString())) {
+            tpl[i]=tpl[i]+1;
+            tw[i]=tw[i]+1;
+            td[i]=td[i];
+            tl[i]=tl[i];
+            ts[i]=parseInt(ts[i])+goal1;
+            ta[i]=parseInt(ta[i])+goal2;
+            tdif[i]=ts[i]-ta[i];
+            tpo[i]=tpo[i]+3;
+
+            var p;
+            if(i==0 || i==1)
+            {
+              p=0;
+            }
+            if(i==2 || i==3)
+            {
+              p=1;
+            }
+            if(i==4 || i==5)
+            {
+              p=2;
+            }
+            ppl[p]=ppl[p]+1;
+            pw[p]=pw[p]+1;
+            pd[p]=pd[p];
+            pl[p]=pl[p];
+            ps[p]=parseInt(ps[p])+goal1;
+            pa[p]=parseInt(pa[p])+goal2;
+            pdif[p]=ps[p]-pa[p];
+            ppo[p]=ppo[p]+3;
+          }
+          if (team(id2).includes((i+1).toString())) {
+            tpl[i]=tpl[i]+1;
+            tw[i]=tw[i];
+            td[i]=td[i];
+            tl[i]=tl[i]+1;
+            ts[i]=parseInt(ts[i])+goal2;
+            ta[i]=parseInt(ta[i])+goal1;
+            tdif[i]=ts[i]-ta[i];
+            tpo[i]=tpo[i];
+
+            var p;
+            if(i==0 || i==1)
+            {
+              p=0;
+            }
+            if(i==2 || i==3)
+            {
+              p=1;
+            }
+            if(i==4 || i==5)
+            {
+              p=2;
+            }
+            ppl[p]=ppl[p]+1;
+            pw[p]=pw[p];
+            pd[p]=pd[p];
+            pl[p]=pl[p]+1;
+            ps[p]=parseInt(ps[p])+goal2;
+            pa[p]=parseInt(pa[p])+goal1;
+            pdif[p]=ps[p]-pa[p];
+            ppo[p]=ppo[p];
+          }
+        }
+}
 //  control every card separetly, color change,input check  //
 function focus_out(id1, id2, id3) {
   if ($(id1).val() != "" && $(id2).val() != "") {
     if (id3.includes("qc")) {
-      $("#" + id3 + ".card_back").css({ "background-color": "rgb(49 247 0 / 35%)" });
-      
-      
+      if ($(id1).val() == $(id2).val()) {
+        draw(id1,id2);
+      }
+      else if ($(id1).val() >= $(id2).val()) {
+        win(id1,id2);
+      }
+      else{
+        win(id2,id1);
+      }
+      // console.log(ppl[1]);
+      player_refresh();
+      team_refresh();
+      qualifier = qualifier + 1;
+      $("#" + id3 + ".card_back").css({
+        "background-color": "rgb(49 247 0 / 35%)",
+      });
     } else if (id3.includes("kc")) {
-      $("#" + id3 + ".card_back").css({ "background-color": "rgb(232 248 6 / 35%)" });
+      $("#" + id3 + ".card_back").css({
+        "background-color": "rgb(232 248 6 / 35%)",
+      });
     } else {
-      $("#" + id3 + ".card_back").css({ "background-color": "rgb(255 0 0 / 35%)" });
+      $("#" + id3 + ".card_back").css({
+        "background-color": "rgb(255 0 0 / 35%)",
+      });
 
-      console.log(compare(id1,id2));
+      console.log(compare(id1, id2));
     }
-    $(id1).prop( "disabled", true );
-    $(id2).prop( "disabled", true );
+    $(id1).prop("disabled", true);
+    $(id2).prop("disabled", true);
   } else {
-    $("#" + id3 + ".card_back").css({ "background-color": "rgb(10 20 34 / 35%)" });  
-    $(id1).prop( "disabled", false );
-    $(id2).prop( "disabled", false );
+    $("#" + id3 + ".card_back").css({
+      "background-color": "rgb(10 20 34 / 35%)",
+    });
+    $(id1).prop("disabled", false);
+    $(id2).prop("disabled", false);
+  }
+  if(qualifier >= 12)
+  {
+    
   }
 }
 function value_return(variable) {
@@ -98,11 +265,64 @@ function value_return(variable) {
     return "in11";
   }
 }
+
+function team_refresh()
+{
+  for (i = 1; i <= 6; i++) {
+    $("#plt" + i + "").text(tpl[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#wt" + i + "").text(tw[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#dt" + i + "").text(td[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#lt" + i + "").text(tl[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#st" + i + "").text(ts[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#at" + i + "").text(ta[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#dift" + i + "").text(tdif[i - 1]);
+  }
+  for (i = 1; i <= 6; i++) {
+    $("#pot" + i + "").text(tpo[i - 1]);
+  }
+}
+function player_refresh()
+{
+  for (i = 1; i <= 3; i++) {
+    $("#pl" + i + "").text(ppl[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#w" + i + "").text(pw[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#d" + i + "").text(pd[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#l" + i + "").text(pl[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#s" + i + "").text(ps[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#a" + i + "").text(pa[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#dif" + i + "").text(pdif[i - 1]);
+  }
+  for (i = 1; i <= 3; i++) {
+    $("#po" + i + "").text(ppo[i - 1]);
+  }
+}
 // initilize jqery data table //
 $(document).ready(function () {
-    
-
-        console.log(a['real'][0]);
+  console.log(a["real"][0]);
   //asign team
   var player1;
   var player1;
@@ -127,12 +347,12 @@ $(document).ready(function () {
   });
 
   $("#number_number").show();
-  $("#player_name").hide();
-  $("#team_name").hide();
-  $("#team_number").hide();
-  $("#page1").hide();
-  $("#page2").hide();
-  $("#page3").hide();
+  // $("#player_name").hide();
+  // $("#team_name").hide();
+  // $("#team_number").hide();
+  // $("#page1").hide();
+  // $("#page2").hide();
+  // $("#page3").hide();
 
   $("#score_table_btn").click(function () {
     $("#page1").hide();
@@ -197,7 +417,7 @@ $(document).ready(function () {
           var r = Math.floor(Math.random() * 6) + 1;
           if (arr.indexOf(r) === -1) arr.push(r);
         }
-        console.log(value_return(arr['real']));
+        console.log(value_return(arr["real"]));
         P1T1 = $("#" + value_return(arr[0])).val();
         P1T2 = $("#" + value_return(arr[1])).val();
         P2T1 = $("#" + value_return(arr[2])).val();
@@ -224,74 +444,43 @@ $(document).ready(function () {
       console.log("T5= " + P3T1);
       console.log("T6= " + P3T2);
 
-      $("#tp_1_1").attr("src",a[P1T1][0]);
-      $("#tp_1_2").attr("src",a[P1T1][0]);
-      $("#tp_1_3").attr("src",a[P1T1][0]);
-      $("#tp_1_4").attr("src",a[P1T1][0]);
+      $("#tp_1_1").attr("src", a[P1T1][0]);
+      $("#tp_1_2").attr("src", a[P1T1][0]);
+      $("#tp_1_3").attr("src", a[P1T1][0]);
+      $("#tp_1_4").attr("src", a[P1T1][0]);
 
-      $("#tp_2_1").attr("src",a[P1T2][0]);
-      $("#tp_2_2").attr("src",a[P1T2][0]);
-      $("#tp_2_3").attr("src",a[P1T2][0]);
-      $("#tp_2_4").attr("src",a[P1T2][0]);
+      $("#tp_2_1").attr("src", a[P1T2][0]);
+      $("#tp_2_2").attr("src", a[P1T2][0]);
+      $("#tp_2_3").attr("src", a[P1T2][0]);
+      $("#tp_2_4").attr("src", a[P1T2][0]);
 
-      $("#tp_3_1").attr("src",a[P2T1][0]);
-      $("#tp_3_2").attr("src",a[P2T1][0]);
-      $("#tp_3_3").attr("src",a[P2T1][0]);
-      $("#tp_3_4").attr("src",a[P2T1][0]);
+      $("#tp_3_1").attr("src", a[P2T1][0]);
+      $("#tp_3_2").attr("src", a[P2T1][0]);
+      $("#tp_3_3").attr("src", a[P2T1][0]);
+      $("#tp_3_4").attr("src", a[P2T1][0]);
 
-      $("#tp_4_1").attr("src",a[P2T2][0]);
-      $("#tp_4_2").attr("src",a[P2T2][0]);
-      $("#tp_4_3").attr("src",a[P2T2][0]);
-      $("#tp_4_4").attr("src",a[P2T2][0]);
+      $("#tp_4_1").attr("src", a[P2T2][0]);
+      $("#tp_4_2").attr("src", a[P2T2][0]);
+      $("#tp_4_3").attr("src", a[P2T2][0]);
+      $("#tp_4_4").attr("src", a[P2T2][0]);
 
-      $("#tp_5_1").attr("src",a[P3T1][0]);
-      $("#tp_5_2").attr("src",a[P3T1][0]);
-      $("#tp_5_3").attr("src",a[P3T1][0]);
-      $("#tp_5_4").attr("src",a[P3T1][0]);
+      $("#tp_5_1").attr("src", a[P3T1][0]);
+      $("#tp_5_2").attr("src", a[P3T1][0]);
+      $("#tp_5_3").attr("src", a[P3T1][0]);
+      $("#tp_5_4").attr("src", a[P3T1][0]);
 
-      $("#tp_6_1").attr("src",a[P3T2][0]);
-      $("#tp_6_2").attr("src",a[P3T2][0]);
-      $("#tp_6_3").attr("src",a[P3T2][0]);
-      $("#tp_6_4").attr("src",a[P3T2][0]);
-////////////////////////////////////////////////      
+      $("#tp_6_1").attr("src", a[P3T2][0]);
+      $("#tp_6_2").attr("src", a[P3T2][0]);
+      $("#tp_6_3").attr("src", a[P3T2][0]);
+      $("#tp_6_4").attr("src", a[P3T2][0]);
+      ////////////////////////////////////////////////
       $("#p1").text(player1);
       $("#p2").text(player2);
       $("#p3").text(player3);
 
-      for(i=1;i<=3;i++)
-      {
-        $("#pl"+i+"").text(ppl[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#w"+i+"").text(pw[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#d"+i+"").text(pd[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#l"+i+"").text(pl[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#s"+i+"").text(ps[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#a"+i+"").text(pa[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#dif"+i+"").text(pdif[i-1]);
-      }
-      for(i=1;i<=3;i++)
-      {
-        $("#po"+i+"").text(ppo[i-1]);
-      }
-      
-//////////////////////////////////////
+      player_refresh();
+
+      //////////////////////////////////////
       $("#pt1").text(P1T1);
       $("#pt2").text(P1T2);
       $("#pt3").text(P2T1);
@@ -299,51 +488,17 @@ $(document).ready(function () {
       $("#pt5").text(P3T1);
       $("#pt6").text(P3T2);
 
-      for(i=1;i<=6;i++)
-      {
-        $("#plt"+i+"").text(tpl[i-1]);
+      team_refresh();
+
+      ///////////////////////
+      for (i = 1; i <= 8; i++) {
+        $("#tpn_1_" + i + "").text(player1);
       }
-      for(i=1;i<=6;i++)
-      {
-        $("#wt"+i+"").text(tw[i-1]);
+      for (i = 1; i <= 8; i++) {
+        $("#tpn_2_" + i + "").text(player2);
       }
-      for(i=1;i<=6;i++)
-      {
-        $("#dt"+i+"").text(td[i-1]);
-      }
-      for(i=1;i<=6;i++)
-      {
-        $("#lt"+i+"").text(tl[i-1]);
-      }
-      for(i=1;i<=6;i++)
-      {
-        $("#st"+i+"").text(ts[i-1]);
-      }
-      for(i=1;i<=6;i++)
-      {
-        $("#at"+i+"").text(ta[i-1]);
-      }
-      for(i=1;i<=6;i++)
-      {
-        $("#dift"+i+"").text(tdif[i-1]);
-      }
-      for(i=1;i<=6;i++)
-      {
-        $("#pot"+i+"").text(tpo[i-1]);
-      }
-      
-///////////////////////
-      for(i=1;i<=8;i++)
-      {
-        $("#tpn_1_"+i+"").text(player1);
-      }      
-      for(i=1;i<=8;i++)
-      {
-        $("#tpn_2_"+i+"").text(player2);
-      }
-      for(i=1;i<=8;i++)
-      {
-        $("#tpn_3_"+i+"").text(player3);
+      for (i = 1; i <= 8; i++) {
+        $("#tpn_3_" + i + "").text(player3);
       }
 
       $("body").css(
