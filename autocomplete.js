efdfdcd11954a04d6c04c379002525c098dcf0f1
Rubyet <rittick.2012@gmail.com>
2021-04-09 21:26:21 +0600

    composer.json added for heroku

--- autocomplete.js
+++ autocomplete.js
@@ -1,61 +1,349 @@
 $( function() {
-    var availableTags = [
-      "Manchester City",
-      "Liverpool",
-      "Real Madrid",
-      "FC Bayern München",
-      "FC Barcelona",
-      "Juventus",
-      "Paris Saint-Germain",
-      "Juventus",
-    ];
-    $( "#in6" ).autocomplete({
-      source: availableTags
-    });
-    $( "#in7" ).autocomplete({
-      source: availableTags
-    });
-    $( "#in8" ).autocomplete({
-      source: availableTags
-    });
-    $( "#in9" ).autocomplete({
-      source: availableTags
-    });
-    $( "#in10" ).autocomplete({
-      source: availableTags
-    });
-    $( "#in11" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  var availableTags = [
+    "Manchester City",
+    "Liverpool",
+    "Real Madrid",
+    "FC Bayern München",
+    "FC Barcelona",
+    "Juventus",
+    "Paris Saint-Germain",
+    "Atlético de Madrid",
+    "Tottenham Hotspur",
+    "Borussia Dortmund",
+    "Manchester United",
+    "Inter",
+    "Chelsea",
+    "Sevilla FC",
+    "Lazio",
+    "Leicester City",
+    "Napoli",
+    "Arsenal",
+    "Villarreal CF",
+    "RB Leipzig",
+    "Everton",
+    "Real Sociedad",
+    "Milan",
+    "Atalanta",
+    "Wolverhampton Wanderers",
+    "Athletic Club",
+    "Bayer 04 Leverkusen",
+    "Borussia Mönchengladbach",
+    "SL Benfica",
+    "West Ham United",
+    "Roma",
+    "Ajax",
+    "Real Betis Balompié",
+    "FC Porto",
+    "Olympique Lyonnais",
+    "LOSC Lille",
+    "Sporting CP",
+    "AS Monaco",
+    "VfL Wolfsburg",
+    "Eintracht Frankfurt",
+    "Olympique de Marseille",
+    "Southampton",
+    "Granada CF",
+    "Getafe CF",
+    "Valencia CF",
+    "Aston Villa",
+    "Atlético Mineiro",
+    "Levante UD",
+    "PSV",
+    "River Plate",
+    "Fiorentina",
+    "Hertha Berlin",
+    "TSG Hoffenheim",
+    "Flamengo",
+    "Newcastle United",
+    "D. Alavés",
+    "CA Osasuna",
+    "Crystal Palace",
+    "R. Valladolid CF",
+    "Olympiacos CFP",
+    "Burnley",
+    "Torino",
+    "Boca Juniors",
+    "SD Eibar",
+    "Stade Rennais FC",
+    "Cagliari",
+    "RC Celta",
+    "Montpellier Hérault SC",
+    "Shakhtar Donetsk",
+    "São Paulo",
+    "Sassuolo",
+    "RCD Espanyol",
+    "Leeds United",
+    "Brighton & Hove Albion",
+    "Feyenoord",
+    "Tigres de la UANL",
+    "Sampdoria",
+    "Galatasaray SK",
+    "Angers SCO",
+    "OGC Nice",
+    "Palmeiras",
+    "İstanbul Başakşehir FK",
+    "Spartak Moscow",
+    "SC Braga",
+    "Fenerbahçe SK",
+    "FC Schalke 04",
+    "Hellas Verona",
+    "Udinese",
+    "PFC CSKA Moscow",
+    "Parma",
+    "RC Strasbourg Alsace",
+    "FC Köln",
+    "Grêmio",
+    "Bologna",
+    "Rangers FC",
+    "Rayados de Monterrey",
+    "FC Girondins de",
+    "FC Augsburg",
+    "Watford",
+    "West Bromwich Albion",
+    "Club Brugge KV",
+    "Fulham",
+    "Sport-Club Freiburg",
+    "Racing Club",
+    "AFC Bournemouth",
+    "FC Nantes",
+    "Lokomotiv Moskva",
+    "SV Werder Bremen",
+    "Cruz Azul",
+    "Sheffield United",
+    "Genoa",
+    "Internacional",
+    "Dynamo Kyiv",
+    "Guangzhou Evergrande Taobao FC",
+    "Stade de Reims",
+    "Norwich City",
+    "FC Red Bull Salzburg",
+    "VfB Stuttgart",
+    "AS Saint-Étienne",
+    "Club Atlético Independiente",
+    "Vélez Sarsfield",
+    "Club América",
+    "FC Union Berlin",
+    "Santos",
+    "Vitória Guimarães",
+    "AZ",
+    "Al Hilal",
+    "Dinamo Zagreb",
+    "Celtic",
+    "Elche CF",
+    "RC Lens",
+    "SD Huesca",
+    "Club León",
+    "PAOK",
+    "FSV Mainz 05",
+    "Sparta Praha",
+    "CD Leganés",
+    "RCD Mallorca",
+    "Nîmes Olympique",
+    "DSC Arminia Bielefeld",
+    "Ceará Sporting Club",
+    "Cádiz CF",
+    "Royal Antwerp FC",
+    "Trabzonspor",
+    "KRC Genk",
+    "Club Athletico Paranaense",
+    "Stade Brestois 29",
+    "Brentford",
+    "San Lorenzo de Almagro",
+    "AEK Athens",
+    "KAA Gent",
+    "UD Almería",
+    "Fortaleza",
+    "Viktoria Plzeň",
+    "BSC Young Boys",
+    "Swansea City",
+    "Rio Ave FC",
+    "Benevento",
+    "FC København",
+    "RSC Anderlecht",
+    "tttttttttttttt",
 
-  var a = [];
-  a['Manchester City'] = ['/img/teams_logo/10.webp']; 
-  a['Liverpool'] = ['/img/teams_logo/9.webp'];
-  a['Real Madrid'] = ['/img/teams_logo/243.webp'];
-  a['FC Bayern München'] = ['/img/teams_logo/21.webp'];
-  a['FC Barcelona'] = ['/img/teams_logo/241.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Paris Saint-Germain'] = ['/img/teams_logo/73.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
-  a['Juventus'] = ['/img/teams_logo/45.webp'];
 
+  ];
+  $( "#in6" ).autocomplete({
+    source: availableTags
+  });
+  $( "#in7" ).autocomplete({
+    source: availableTags
+  });
+  $( "#in8" ).autocomplete({
+    source: availableTags
+  });
+  $( "#in9" ).autocomplete({
+    source: availableTags
+  });
+  $( "#in10" ).autocomplete({
+    source: availableTags
+  });
+  $( "#in11" ).autocomplete({
+    source: availableTags
+  });
+} );
 
-  
+var a = [];
+a['Manchester City'] = ['/img/teams_logo/10.webp']; 
+a['Liverpool'] = ['/img/teams_logo/9.webp'];
+a['Real Madrid'] = ['/img/teams_logo/243.webp'];
+a['FC Bayern München'] = ['/img/teams_logo/21.webp'];
+a['FC Barcelona'] = ['/img/teams_logo/241.webp'];
+a['Juventus'] = ['/img/teams_logo/45.webp'];
+a['Paris Saint-Germain'] = ['/img/teams_logo/73.webp'];
+a['Atlético de Madrid'] = ['/img/teams_logo/240.webp'];
+a['Tottenham Hotspur'] = ['/img/teams_logo/18.webp'];
+a['Borussia Dortmund'] = ['/img/teams_logo/22.webp'];
+a['Manchester United'] = ['/img/teams_logo/11.webp'];
+a['Inter'] = ['/img/teams_logo/44.webp'];
+a['Chelsea'] = ['/img/teams_logo/5.webp'];
+a['Sevilla FC'] = ['/img/teams_logo/481.webp'];
+a['Lazio'] = ['/img/teams_logo/46.webp'];
+a['Leicester City'] = ['/img/teams_logo/95.webp'];
+a['Napoli'] = ['/img/teams_logo/48.webp'];
+a['Arsenal'] = ['/img/teams_logo/1.webp'];
+a['Villarreal CF'] = ['/img/teams_logo/483.webp'];
+a['RB Leipzig'] = ['/img/teams_logo/112172.webp'];
+a['Everton'] = ['/img/teams_logo/7.webp'];
+a['Real Sociedad'] = ['/img/teams_logo/457.webp'];
+a['Milan'] = ['/img/teams_logo/47.webp'];
+a['Atalanta'] = ['/img/teams_logo/39.webp'];
+a['Wolverhampton Wanderers'] = ['/img/teams_logo/110.webp'];
+a['Athletic Club'] = ['/img/teams_logo/448.webp'];
+a['Bayer 04 Leverkusen'] = ['/img/teams_logo/32.webp'];
+a['Borussia Mönchengladbach'] = ['/img/teams_logo/23.webp'];
+a['SL Benfica'] = ['/img/teams_logo/234.webp'];
+a['West Ham United'] = ['/img/teams_logo/19.webp'];
+a['Roma'] = ['/img/teams_logo/52.webp'];
+a['Ajax'] = ['/img/teams_logo/245.webp'];
+a['Real Betis Balompié'] = ['/img/teams_logo/449.webp'];
+a['FC Porto'] = ['/img/teams_logo/236.webp'];
+a['Olympique Lyonnais'] = ['/img/teams_logo/66.webp'];
+a['LOSC Lille'] = ['/img/teams_logo/65.webp'];
+a['Sporting CP'] = ['/img/teams_logo/237.webp'];
+a['AS Monaco'] = ['/img/teams_logo/69.webp'];
+a['VfL Wolfsburg'] = ['/img/teams_logo/175.webp'];
+a['Eintracht Frankfurt'] = ['/img/teams_logo/1824.webp'];
+a['Olympique de Marseille'] = ['/img/teams_logo/219.webp'];
+a['Southampton'] = ['/img/teams_logo/17.webp'];
+a['Granada CF'] = ['/img/teams_logo/110832.webp'];
+a['Getafe CF'] = ['/img/teams_logo/1860.webp'];
+a['Valencia CF'] = ['/img/teams_logo/461.webp'];
+a['Aston Villa'] = ['/img/teams_logo/2.webp'];
+a['Atlético Mineiro'] = ['/img/teams_logo/1035.webp'];
+a['Levante UD'] = ['/img/teams_logo/1853.webp'];
+a['PSV'] = ['/img/teams_logo/247.webp'];
+a['River Plate'] = ['/img/teams_logo/1876.webp'];
+a['Fiorentina'] = ['/img/teams_logo/110374.webp'];
+a['Hertha Berlin'] = ['/img/teams_logo/166.webp'];
+a['TSG Hoffenheim'] = ['/img/teams_logo/10029.webp'];
+a['Flamengo'] = ['/img/teams_logo/1043.webp'];
+a['Newcastle United'] = ['/img/teams_logo/13.webp'];
+a['D. Alavés'] = ['/img/teams_logo/463.webp'];
+a['CA Osasuna'] = ['/img/teams_logo/479.webp'];
+a['Crystal Palace'] = ['/img/teams_logo/1799.webp'];
+a['R. Valladolid CF'] = ['/img/teams_logo/462.webp'];
+a['Olympiacos CFP'] = ['/img/teams_logo/280.webp'];
+a['Burnley'] = ['/img/teams_logo/1796.webp'];
+a['Torino'] = ['/img/teams_logo/54.webp'];
+a['Boca Juniors'] = ['/img/teams_logo/1877.webp'];
+a['SD Eibar'] = ['/img/teams_logo/467.webp'];
+a['Stade Rennais FC'] = ['/img/teams_logo/74.webp'];
+a['Cagliari'] = ['/img/teams_logo/1842.webp'];
+a['RC Celta'] = ['/img/teams_logo/450.webp'];
+a['Montpellier Hérault SC'] = ['/img/teams_logo/70.webp'];
+a['Shakhtar Donetsk'] = ['/img/teams_logo/101059.webp'];
+a['São Paulo'] = ['/img/teams_logo/598.webp'];
+a['Sassuolo'] = ['/img/teams_logo/111974.webp'];
+a['RCD Espanyol'] = ['/img/teams_logo/452.webp'];
+a['Leeds United'] = ['/img/teams_logo/8.webp'];
+a['Brighton & Hove Albion'] = ['/img/teams_logo/1808.webp'];
+a['Feyenoord'] = ['/img/teams_logo/246.webp'];
+a['Tigres de la UANL'] = ['/img/teams_logo/1970.webp'];
+a['Sampdoria'] = ['/img/teams_logo/1837.webp'];
+a['Galatasaray SK'] = ['/img/teams_logo/325.webp'];
+a['Angers SCO'] = ['/img/teams_logo/1530.webp'];
+a['OGC Nice'] = ['/img/teams_logo/72.webp'];
+a['Palmeiras'] = ['/img/teams_logo/383.webp'];
+a['İstanbul Başakşehir FK'] = ['/img/teams_logo/101014.webp'];
+a['Spartak Moscow'] = ['/img/teams_logo/100767.webp'];
+a['SC Braga'] = ['/img/teams_logo/1896.webp'];
+a['Fenerbahçe SK'] = ['/img/teams_logo/326.webp'];
+a['FC Schalke 04'] = ['/img/teams_logo/34.webp'];
+a['Hellas Verona'] = ['/img/teams_logo/206.webp'];
+a['Udinese'] = ['/img/teams_logo/55.webp'];
+a['PFC CSKA Moscow'] = ['/img/teams_logo/315.webp'];
+a['Parma'] = ['/img/teams_logo/50.webp'];
+a['RC Strasbourg Alsace'] = ['/img/teams_logo/76.webp'];
+a['FC Köln'] = ['/img/teams_logo/31.webp'];
+a['Grêmio'] = ['/img/teams_logo/1629.webp'];
+a['Bologna'] = ['/img/teams_logo/189.webp'];
+a['Rangers FC'] = ['/img/teams_logo/86.webp'];
+a['Rayados de Monterrey'] = ['/img/teams_logo/1032.webp'];
+a['FC Girondins de'] = ['/img/teams_logo/59.webp'];
+a['FC Augsburg'] = ['/img/teams_logo/100409.webp'];
+a['Watford'] = ['/img/teams_logo/1795.webp'];
+a['West Bromwich Albion'] = ['/img/teams_logo/109.webp'];
+a['Club Brugge KV'] = ['/img/teams_logo/231.webp'];
+a['Fulham'] = ['/img/teams_logo/144.webp'];
+a['Sport-Club Freiburg'] = ['/img/teams_logo/25.webp'];
+a['Racing Club'] = ['/img/teams_logo/101085.webp'];
+a['AFC Bournemouth'] = ['/img/teams_logo/1943.webp'];
+a['FC Nantes'] = ['/img/teams_logo/71.webp'];
+a['Lokomotiv Moskva'] = ['/img/teams_logo/100765.webp'];
+a['SV Werder Bremen'] = ['/img/teams_logo/38.webp'];
+a['Cruz Azul'] = ['/img/teams_logo/1878.webp'];
+a['Sheffield United'] = ['/img/teams_logo/1794.webp'];
+a['Genoa'] = ['/img/teams_logo/110556.webp'];
+a['Internacional'] = ['/img/teams_logo/1048.webp'];
+a['Dynamo Kyiv'] = ['/img/teams_logo/101047.webp'];
+a['Guangzhou Evergrande Taobao FC'] = ['/img/teams_logo/111839.webp'];
+a['Stade de Reims'] = ['/img/teams_logo/379.webp'];
+a['Norwich City'] = ['/img/teams_logo/1792.webp'];
+a['FC Red Bull Salzburg'] = ['/img/teams_logo/191.webp'];
+a['VfB Stuttgart'] = ['/img/teams_logo/36.webp'];
+a['AS Saint-Étienne'] = ['/img/teams_logo/1819.webp'];
+a['Club Atlético Independiente'] = ['/img/teams_logo/110093.webp'];
+a['Vélez Sarsfield'] = ['/img/teams_logo/101088.webp'];
+a['Club América'] = ['/img/teams_logo/1879.webp'];
+a['FC Union BerlinFC Union Berlin'] = ['/img/teams_logo/1831.webp'];
+a['Santos'] = ['/img/teams_logo/1053.webp'];
+a['Vitória Guimarães'] = ['/img/teams_logo/1887.webp'];
+a['AZ'] = ['/img/teams_logo/1906.webp'];
+a['Al Hilal'] = ['/img/teams_logo/605.webp'];
+a['Dinamo Zagreb'] = ['/img/teams_logo/211.webp'];
+a['Celtic'] = ['/img/teams_logo/78.webp'];
+a['Elche CF'] = ['/img/teams_logo/468.webp'];
+a['RC Lens'] = ['/img/teams_logo/64.webp'];
+a['SD Huesca'] = ['/img/teams_logo/110839.webp'];
+a['Club León'] = ['/img/teams_logo/110781.webp'];
+a['PAOK'] = ['/img/teams_logo/393.webp'];
+a['FSV Mainz 05'] = ['/img/teams_logo/169.webp'];
+a['Sparta Praha'] = ['/img/teams_logo/267.webp'];
+a['CD Leganés'] = ['/img/teams_logo/100888.webp'];
+a['RCD Mallorca'] = ['/img/teams_logo/453.webp'];
+a['Nîmes Olympique'] = ['/img/teams_logo/224.webp'];
+a['DSC Arminia Bielefeld'] = ['/img/teams_logo/159.webp'];
+a['Ceará Sporting Club'] = ['/img/teams_logo/111059.webp'];
+a['Cádiz CF'] = ['/img/teams_logo/1968.webp'];
+a['Royal Antwerp FC'] = ['/img/teams_logo/230.webp'];
+a['Trabzonspor'] = ['/img/teams_logo/436.webp'];
+a['KRC Genk'] = ['/img/teams_logo/673.webp'];
+a['Club Athletico Paranaense'] = ['/img/teams_logo/1039.webp'];
+a['Stade Brestois 29'] = ['/img/teams_logo/378.webp'];
+a['Brentford'] = ['/img/teams_logo/1925.webp'];
+a['San Lorenzo de Almagro'] = ['/img/teams_logo/1013.webp'];
+a['AEK Athens'] = ['/img/teams_logo/278.webp'];
+a['KAA Gent'] = ['/img/teams_logo/674.webp'];
+a['UD Almería'] = ['/img/teams_logo/1861.webp'];
+a['Fortaleza'] = ['/img/teams_logo/111052.webp'];
+a['Viktoria Plzeň'] = ['/img/teams_logo/110468.webp'];
+a['BSC Young Boys'] = ['/img/teams_logo/900.webp'];
+a['Swansea City'] = ['/img/teams_logo/1960.webp'];
+a['Rio Ave FC'] = ['/img/teams_logo/744.webp'];
+a['Benevento'] = ['/img/teams_logo/112026.webp'];
+a['FC København'] = ['/img/teams_logo/819.webp'];
+a['RSC Anderlecht'] = ['/img/teams_logo/229.webp'];
+a['tttttt'] = ['/img/teams_logo/ppppp'];
+
